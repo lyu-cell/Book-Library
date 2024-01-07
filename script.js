dab6c02cb3c88454500b786365f74fd58907dc3b
lyu-cell <shizueizawa340@gmail.com>
2024-01-08 01:24:18 +0600

    prototype layout and the basic core functions and constructors is complete

---
+++ script.js
@@ -0,0 +1,23 @@
+let array = [];
+
+function Book(name, author, page, state) {
+    this.name = name;
+    this.author = author;
+    this.page = page;
+    this.state = state;
+    this.logInfo = function () {
+        return `${this.name}, by ${this.author}, ${this.page} pages, ${this.state}`
+    }
+};
+
+
+function addArray () {
+    
+    let Name = prompt("Write the name of the book");
+    let author = prompt("Author name?");
+    let pages = prompt("How many pages the book have?");
+    let readOrNot = prompt("if you've read it then type 'Read' if not then 'Unread'")
+    
+    const book = new Book(Name, author, pages, readOrNot);
+    array.push(book)
+};
